2e804cdfdfb9b23ed0f7ff8c959bea77323a9254
PJ <143397172+PiyushJaiswal236@users.noreply.github.com>
2025-02-26 08:55:43 +0530

    Update bluetooth_classic.dart

--- lib/bluetooth_classic.dart
+++ lib/bluetooth_classic.dart
@@ -64,4 +64,8 @@ class BluetoothClassic {
     return BluetoothClassicPlatform.instance.writeRawBytes(data);
   }
 
+  Future<bool> writeTwoUint8Lists(Uint8List data1, Uint8List data2) {
+    return BluetoothClassicPlatform.instance.writeTwoUint8Lists( data1,  data2);
+  }
+
 }
